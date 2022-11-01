49a499ae42268de1b2066f7e6913969fe3366f44
GeorgeWestwater <george.westwater@progress.com>
2022-11-01 08:39:39 -0500

    effortless package allows dynamic node id

--- purge.sh
+++ purge.sh
@@ -2,6 +2,20 @@
 
 #rm cloud-formation.yml
 #rm README.md
+#rm -rf init/00-proxy/results
+
+rm -rf shim/00-proxy/results
+rm -rf shim/00-workshop-instructions/results
+rm -rf shim/01-build-and-package/results
+rm -rf shim/02-deploy-rollback/results
+rm -rf shim/03-cots/results
+rm -rf shim/04-decentralized/results
+rm -rf shim/05-ring/results
+rm -rf shim/06-smart-updates/results
+rm -rf shim/07-host-compliance/results
+rm -rf shim/08-host-configuration/results
+rm -rf shim/09-app-compliance/results
+rm -rf shim/10-jenkins/results
 
 rm -rf workshop/00-proxy/results
 rm -rf workshop/00-workshop-instructions/results
--- workshop/07-host-compliance/habitat/plan.sh
+++ workshop/07-host-compliance/habitat/plan.sh
@@ -1,6 +1,31 @@
-pkg_origin=workshop
 pkg_name=host_compliance
 pkg_version=1.0.0
-pkg_scaffolding="chef/scaffolding-chef-inspec"
+pkg_origin=workshop
+pkg_deps=(chef/inspec)
 pkg_svc_user="root"
 pkg_svc_group="root"
+
+do_setup_environment() {
+  ARCHIVE_PATH="$HAB_CACHE_SRC_PATH/$pkg_dirname/$pkg_name-$pkg_version.tar.gz"
+}
+
+do_build() {
+  if [ ! -f $PLAN_CONTEXT/../inspec.yml ]; then
+    exit_with 'Cannot find inspec.yml. Please build from profile root.' 1
+  fi
+
+  local profile_files=($(ls $PLAN_CONTEXT/../ -I habitat -I results))
+  local profile_location="$HAB_CACHE_SRC_PATH/$pkg_dirname/build"
+  mkdir -p $profile_location
+
+  build_line "Copying profile files to $profile_location"
+  cp -R ${profile_files[@]} $profile_location
+
+  build_line "Archiving $ARCHIVE_PATH"
+  inspec archive "$HAB_CACHE_SRC_PATH/$pkg_dirname/build" -o $ARCHIVE_PATH --overwrite
+}
+
+do_install() {
+  mkdir -p $pkg_prefix/profiles
+  cp $ARCHIVE_PATH $pkg_prefix/profiles
+}
