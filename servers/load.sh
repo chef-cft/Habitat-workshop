74b2357c3e63b61a51aa3277baa5e1d2bcf990c7
GeorgeWestwater <george.westwater@progress.com>
2022-11-01 20:22:33 +0000

    minor script changes

--- servers/load.sh
+++ servers/load.sh
@@ -1,3 +1,13 @@
-startup/store-1.sh
-startup/store-2.sh
-startup/store-3.sh
+if test -z "$STUDIO_TYPE" 
+then
+    echo "must run inside of studio"
+    exit -1
+fi
+
+. /src/env.sh
+
+export HAB_CTL_SECRET="Ct6xkqw4kggECo1IvzappT1v5tc/T0833k42H8gvTSARPou9MIka4LURSwZQlrp7q9FJMZ4hT0pnihymcKGcLg=="
+
+/src/servers/startup/store-1.sh
+/src/servers/startup/store-2.sh
+/src/servers/startup/store-3.sh
--- servers/unload.sh
+++ servers/unload.sh
@@ -1,3 +1,14 @@
-shutdown/store-1.sh
-shutdown/store-2.sh
-shutdown/store-3.sh
+if test -z "$STUDIO_TYPE" 
+then
+    echo "must run inside of studio"
+    exit -1
+fi
+
+. /src/env.sh
+
+export HAB_CTL_SECRET="Ct6xkqw4kggECo1IvzappT1v5tc/T0833k42H8gvTSARPou9MIka4LURSwZQlrp7q9FJMZ4hT0pnihymcKGcLg=="
+
+
+/src/servers/shutdown/store-1.sh
+/src/servers/shutdown/store-2.sh
+/src/servers/shutdown/store-3.sh
